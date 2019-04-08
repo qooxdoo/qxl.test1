55995e22f2fbf6f1148ebfd642388faac0f559c6
Christian Boulanger <info@bibliograph.org>
2019-04-08 07:59:57 +0200

    Initial commit

---
+++ source/class/qxl/test1/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright: 2019 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("qxl.test1.theme.Theme",
+{
+  meta :
+  {
+    color : qxl.test1.theme.Color,
+    decoration : qxl.test1.theme.Decoration,
+    font : qxl.test1.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qxl.test1.theme.Appearance
+  }
+});
